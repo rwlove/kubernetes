88fca1776daa17268820c61af2833b3379ef0377
Robert Love <terp4life2001@gmail.com>
2020-05-09 10:55:29 -0700

    hack create

--- create-cluster.sh
+++ create-cluster.sh
@@ -5,25 +5,25 @@ if [ -d ${HOME}/.kube ] ; then
     rm -rf ${HOME}/.kube
 fi
 
-echo "#### Get local IP Address (eno1) ####"
-ip_addr=$(ip addr show dev eno1 | grep inet | grep -v inet6 | awk '{ print $2 }' | cut -d '/' -f 1)
+#echo "#### Get local IP Address (eno1) ####"
+#ip_addr=$(ip addr show dev eno1 | grep inet | grep -v inet6 | awk '{ print $2 }' | cut -d '/' -f 1)
 
-echo "#### Update local IP Address in init.config ####"
-sudo sed "s/MASTER_IP/${ip_addr}/g" < ${HOME}/kubernetes/init.config.orig > ${HOME}/kubernetes/init.config
+#echo "#### Update local IP Address in init.config ####"
+#sed "s/MASTER_IP/${ip_addr}/g" < ${HOME}/kubernetes/init.config.orig > ${HOME}/kubernetes/init.config
 
 echo "#### Initialize the K8S Cluster ####"
-sudo kubeadm init --ignore-preflight-errors=Service-Docker,IsDockerSystemdCheck,SystemVerification --config ${HOME}/kubernetes/init.config
+kubeadm init
 
 echo "#### Copy K8S config ####"
-sudo mkdir ${HOME}/.kube
-sudo cp -f /etc/kubernetes/admin.conf ${HOME}/.kube/config
-sudo chown -R ${USER}.${USER} ${HOME}/.kube
+mkdir ${HOME}/.kube
+cp -f /etc/kubernetes/admin.conf ${HOME}/.kube/config
+chown -R ${USER}.${USER} ${HOME}/.kube
 
-echo "#### Update --node-ip=${ip_addr} in /var/lib/kubelet/kubeadm-flags.env ####"
-sudo sed -i "s/\"$/ --node-ip=${ip_addr}\"/g" /var/lib/kubelet/kubeadm-flags.env
+#echo "#### Update --node-ip=${ip_addr} in /var/lib/kubelet/kubeadm-flags.env ####"
+#sed -i "s/\"$/ --node-ip=${ip_addr}\"/g" /var/lib/kubelet/kubeadm-flags.env
 
-echo "#### Restart the kubelet ####"
-sudo systemctl restart kubelet.service
+#echo "#### Restart the kubelet ####"
+#sudo systemctl restart kubelet.service
 
 echo "#### Untaint Master ####"
 kubectl taint nodes --all node-role.kubernetes.io/master-
