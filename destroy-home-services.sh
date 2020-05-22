ef04d14cca7a13408c1d9bd92343c6aaed634376
Robert Love <terp4life2001@gmail.com>
2020-05-22 11:44:41 -0700

    delete messages

--- destroy-home-services.sh
+++ destroy-home-services.sh
@@ -3,30 +3,52 @@
 KUBE_DELETE='kubectl delete'
 NAMESPACE='-n home-services'
 
-${KUBE_DELETE} -f configmap/lb/metallb.yaml
-
-${KUBE_DELETE} -f manifests/lb/metallb.yaml
-
 #${KUBE_DELETE} -f manifests/lb/nginx-ingress.yaml
 
+echo "############"
+echo "Delete MetalLB"
+echo "######"
+${KUBE_DELETE} -f configmap/lb/metallb.yaml
+${KUBE_DELETE} -f manifests/lb/metallb.yaml
 ${KUBE_DELETE} -f manifests/lb/metallb-namespace.yaml
 
+echo "############"
+echo "Delete dnsutils"
+echo "######"
 ${KUBE_DELETE} -f manifests/tools/dnsutils.yaml
 
+echo "############"
+echo "Delete k8dash"
+echo "######"
 ${KUBE_DELETE} -f manifests/dashboard/kubernetes-k8dash.yaml
 
+echo "############"
+echo "Delete rompr"
+echo "######"
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/rompr/rompr-service.yaml
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/rompr/rompr.yaml
 
+echo "############"
+echo "Delete mpd"
+echo "######"
 ${KUBE_DELETE} -f manifests/services/mpd/mpd-service.yaml
 ${KUBE_DELETE} -f manifests/services/mpd/mpd.yaml
 
+echo "############"
+echo "Delete homeassistant"
+echo "######"
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/homeassistant/homeassistant-service.yaml
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/homeassistant/homeassistant.yaml
 
+echo "############"
+echo "Delete nginx-hello"
+echo "######"
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello-service.yaml
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello.yaml
 
+echo "############"
+echo "Delete pihole"
+echo "######"
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/pihole/pihole-service.yaml
 ${KUBE_DELETE} ${NAMESPACE} -f manifests/services/pihole/pihole.yaml
 
@@ -49,7 +71,6 @@ ${KUBE_DELETE} -f manifests/db/mysql-pv.yaml
 helm uninstall prometheus
 helm uninstall grafana
 
-
 echo "############"
 echo "Delete the subsonic PostgreSQL database"
 echo "######"
@@ -60,16 +81,25 @@ echo "Uninstall the operator."
 echo "######"
 kubectl delete -f operators/postgresql.yaml
 
+echo "############"
+echo "Delete kubemq"
+echo "######"
 ${KUBE_DELETE} -f manifests/services/kubemq/kubemq-dashboard.yaml
 ${KUBE_DELETE} -f manifests/services/kubemq/kubemq-cluster.yaml
 ${KUBE_DELETE} -f operators/kubemq-operator.yaml
 
+echo "############"
+echo "Delete Physical (storage) Volumes"
+echo "######"
 ${KUBE_DELETE} -f manifests/volumes/nextcloud-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/music-volume-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/mariadb-nextcloud-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/mariadb-subsonic-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/postgress-subsonic-pv.yaml
 
+echo "############"
+echo "Delete namespaces (pgo and home-services)"
+echo "######"
 ${KUBE_DELETE} -f manifests/db/crunchy_postgresql/pgo-namespace.yaml
 ${KUBE_DELETE} -f manifests/services/home-services-namespace.yaml
 
