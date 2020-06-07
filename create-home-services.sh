e19cacc34bddc5f657568ba5ad85b2ffcdbf90f2
Robert Love <terp4life2001@gmail.com>
2020-06-07 14:54:46 -0700

    fix

--- create-home-services.sh
+++ create-home-services.sh
@@ -45,7 +45,6 @@ helm -n rabbitmq install rabbitmq \
      persistence.storageClass='rabbitmq-storage-class',\
      service.type='LoadBalancer',\
      service.loadBalancerIP='192.168.6.22',\
-     service.extraPorts='1883',\
      rabbitmq.extraPlugins='rabbitmq_mqtt' \
      bitnami/rabbitmq
 
