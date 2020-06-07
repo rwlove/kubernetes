61d093a27f50d5e9d3ae1bff9fb5ff78924ae92b
Robert Love <terp4life2001@gmail.com>
2020-06-07 14:51:35 -0700

    fix commas

--- create-home-services.sh
+++ create-home-services.sh
@@ -45,7 +45,7 @@ helm -n rabbitmq install rabbitmq \
      persistence.storageClass='rabbitmq-storage-class',\
      service.type='LoadBalancer',\
      service.loadBalancerIP='192.168.6.22',\
-     rabbitmq.extraPlugins='rabbitmq_mqtt',\
+     rabbitmq.extraPlugins='rabbitmq_mqtt'\
      bitnami/rabbitmq
 
 echo "############"
