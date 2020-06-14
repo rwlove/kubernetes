4c9b765f3fdb5a08769f897d773b32117de4babd
Robert Love <terp4life2001@gmail.com>
2020-06-14 07:44:24 -0700

    use billimek

--- create-home-services.sh
+++ create-home-services.sh
@@ -173,7 +173,7 @@ echo "Install Home Assistant from Helm"
 echo "######"
 ${KUBE_CREATE} -f manifests/homeassistant/homeassistant-namespace.yaml
 kubectl -n homeassistant create secret generic git-creds --from-file=ssh-privatekey=/root/.ssh/id_rsa --from-file=ssh-publickey=/root/.ssh/id_rsa.pub
-helm install -n homeassistant homeassistant -f helm/homeassistant.yaml stable/home-assistant
+helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
 echo "############"
 echo "Install grafana with helm"
