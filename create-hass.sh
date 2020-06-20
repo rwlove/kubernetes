cb8d4d6d41606095f2b25e4e76d8d830da7d9ae9
Robert Love <terp4life2001@gmail.com>
2020-06-20 16:52:39 -0700

    fix

--- create-hass.sh
+++ create-hass.sh
@@ -8,8 +8,10 @@ echo "######"
 ${KUBE_CREATE} -f manifests/volumes/homeassistant-pv.yaml
 
 ${KUBE_CREATE} -f manifests/homeassistant/homeassistant-namespace.yaml
-kubectl -n homeassistant create secret generic git-creds --from-file=ssh-privatekey=/root/.ssh/id_rsa --from-file=ssh-publickey=/root/.ssh/id_rsa.pub
-
+ssh-keyscan github.com > /tmp/known_hosts
+kubectl -n homeassistant create secret generic git-creds \
+	--from-file=ssh=$HOME/.ssh/id_rsa \
+	--from-file=known_hosts=/tmp/known_hosts
 helm repo add billimek https://billimek.com/billimek-charts/
 helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
