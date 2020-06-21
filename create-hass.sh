e75c06b283f8d73bcbfaf7c46fa1b4b79b4fff44
Robert Love <terp4life2001@gmail.com>
2020-06-20 17:45:44 -0700

    id_rsa.pub

--- create-hass.sh
+++ create-hass.sh
@@ -11,7 +11,8 @@ ${KUBE_CREATE} -f manifests/homeassistant/homeassistant-namespace.yaml
 ssh-keyscan github.com > /root/.ssh/known_hosts
 kubectl -n homeassistant create secret generic git-creds \
 	--from-file=id_rsa=/root/.ssh/id_rsa \
-	--from-file=known_hosts=/root/.ssh/known_hosts
+	--from-file=known_hosts=/root/.ssh/known_hosts \
+	--from-file=id_rsa.pub=/root/ssh/id_rsa.pub
 helm repo add billimek https://billimek.com/billimek-charts/
 helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
