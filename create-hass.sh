45496d53bfc74b9909b6cb2d907817d2e84a257d
Robert Love <terp4life2001@gmail.com>
2020-06-20 17:46:35 -0700

    fix

--- create-hass.sh
+++ create-hass.sh
@@ -12,7 +12,7 @@ ssh-keyscan github.com > /root/.ssh/known_hosts
 kubectl -n homeassistant create secret generic git-creds \
 	--from-file=id_rsa=/root/.ssh/id_rsa \
 	--from-file=known_hosts=/root/.ssh/known_hosts \
-	--from-file=id_rsa.pub=/root/ssh/id_rsa.pub
+	--from-file=id_rsa.pub=/root/.ssh/id_rsa.pub
 helm repo add billimek https://billimek.com/billimek-charts/
 helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
