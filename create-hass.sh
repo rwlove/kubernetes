362f3c86b75e6e45efc4f00236257c576f4b0e25
Robert Love <terp4life2001@gmail.com>
2020-06-20 17:42:20 -0700

    try without known_hosts

--- create-hass.sh
+++ create-hass.sh
@@ -10,8 +10,7 @@ ${KUBE_CREATE} -f manifests/volumes/homeassistant-pv.yaml
 ${KUBE_CREATE} -f manifests/homeassistant/homeassistant-namespace.yaml
 ssh-keyscan github.com > /root/.ssh/known_hosts
 kubectl -n homeassistant create secret generic git-creds \
-	--from-file=id_rsa=/root/.ssh/id_rsa \
-	--from-file=known_hosts=/root/.ssh/known_hosts
+	--from-file=id_rsa=/root/.ssh/id_rsa
 helm repo add billimek https://billimek.com/billimek-charts/
 helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
