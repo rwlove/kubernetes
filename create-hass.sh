23ea4044c43b67c3626253ca7e9153166e2af5eb
Robert Love <terp4life2001@gmail.com>
2020-06-21 08:03:32 -0700

    fix

--- create-hass.sh
+++ create-hass.sh
@@ -13,8 +13,6 @@ kubectl -n homeassistant create secret generic git-creds \
 	--from-file=id_rsa=/root/.ssh/id_rsa \
 	--from-file=known_hosts=/root/.ssh/known_hosts \
 	--from-file=id_rsa.pub=/root/.ssh/id_rsa.pub
-kubectl -n homeassistant create secret generic config-secrets \
-	--from-file=secrets.yaml=/root/kubernetes/manifests/homeassistant/secrets.yaml
 helm repo add billimek https://billimek.com/billimek-charts/
 helm install -n homeassistant homeassistant -f helm/homeassistant.yaml billimek/home-assistant
 
