8e1d5d3631706e2c906317e703c4c45f4b7b838d
Robert Love <terp4life2001@gmail.com>
2020-07-02 14:38:32 -0700

    add-grocy-note

--- create-grocy.sh
+++ create-grocy.sh
@@ -12,3 +12,6 @@ ${KUBE_CREATE} -n grocy -f manifests/volumes/grocy-pv.yaml
 helm repo add billimek https://billimek.com/billimek-charts/
 
 helm install -n grocy grocy -f helm/grocy.yaml billimek/grocy
+
+echo "Default username: admin"
+echo "Default password: admin"
