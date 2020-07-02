82c709d46526f101b8268cf91d569e98636da827
Robert Love <terp4life2001@gmail.com>
2020-07-02 16:04:28 -0700

    Revert "enable-speedtest-servicemonitor"
    This reverts commit 4d7ad75e9f33f18171e9ca4acf9d53958ec0412f.

--- create-speedtest.sh
+++ create-speedtest.sh
@@ -9,8 +9,4 @@ ${KUBE_CREATE} ns speedtest-prometheus
 
 helm repo add billimek https://billimek.com/billimek-charts/
 
-helm install \
-     -n speedtest-prometheus \
-     speedtest-prometheus \
-     --set serviceMonitor.enabled=true \
-     billimek/speedtest-prometheus
+helm install -n speedtest-prometheus speedtest-prometheus billimek/speedtest-prometheus
