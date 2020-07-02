ae821aa3b0fb47d51e19ff92beedf892ff57459b
Robert Love <terp4life2001@gmail.com>
2020-07-02 13:22:32 -0700

    grocy-pvc

--- create-grocy.sh
+++ create-grocy.sh
@@ -7,6 +7,8 @@ echo "Install Grocy from Helm"
 echo "######"
 ${KUBE_CREATE} ns grocy
 
+${KUBE_CREATE} -n grocy manifests/volumes/kanboard-pv.yaml
+
 helm repo add billimek https://billimek.com/billimek-charts/
 
 helm install -n grocy grocy -f helm/grocy.yaml billimek/grocy
