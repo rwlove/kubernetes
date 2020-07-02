b316036e2028194d3a348076c7f7abe0c55c9675
Robert Love <terp4life2001@gmail.com>
2020-07-02 13:33:28 -0700

    destroy grocy pv

--- destroy-grocy.sh
+++ destroy-grocy.sh
@@ -7,6 +7,8 @@ echo "Uninstall Grocy from Helm"
 echo "######"
 helm -n grocy uninstall grocy
 
+${KUBE_DELETE} -n grocy -f manifests/volumes/grocy-pv.yaml
+
 ${KUBE_DELETE} ns grocy
 
 helm repo remove billimek
