f7c230996281c1b260c1f03a71c015642d38413e
Robert Love <terp4life2001@gmail.com>
2020-07-02 17:16:22 -0700

    rm ns

--- create-pihole.sh
+++ create-pihole.sh
@@ -5,7 +5,6 @@ KUBE_CREATE='kubectl create'
 echo "############"
 echo "Create pihole"
 echo "######"
-${KUBE_CREATE} namespace pihole
 ${KUBE_CREATE} -f manifests/volumes/pihole-pv.yaml
 ${KUBE_CREATE} -n pihole -f manifests/services/pihole/pihole.yaml
 ${KUBE_CREATE} -n pihole -f manifests/services/pihole/pihole-service.yaml
--- destroy-pihole.sh
+++ destroy-pihole.sh
@@ -8,4 +8,3 @@ echo "######"
 ${KUBE_DELETE} -n pihole -f manifests/services/pihole/pihole-service.yaml
 ${KUBE_DELETE} -n pihole -f manifests/services/pihole/pihole.yaml
 ${KUBE_DELETE} -f manifests/volumes/pihole-pv.yaml
-${KUBE_DELETE} namespace pihole
