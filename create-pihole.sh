321c32419a53dbe121f87c904fa2aebd47577a59
Robert Love <terp4life2001@gmail.com>
2020-07-02 17:14:06 -0700

    fix-pihole

--- create-pihole.sh
+++ create-pihole.sh
@@ -6,7 +6,7 @@ echo "############"
 echo "Create pihole"
 echo "######"
 ${KUBE_CREATE} namespace pihole
-${KUBE_CREATE} pihole -f manifests/volumes/pihole-pv.yaml
+${KUBE_CREATE} -f manifests/volumes/pihole-pv.yaml
 ${KUBE_CREATE} -n pihole -f manifests/services/pihole/pihole.yaml
 ${KUBE_CREATE} -n pihole -f manifests/services/pihole/pihole-service.yaml
 
