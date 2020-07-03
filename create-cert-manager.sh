8507059a3f99734367b8a08ab18c2c1f117e1f08
Robert Love <terp4life2001@gmail.com>
2020-07-02 19:20:15 -0700

    fix

--- create-cert-manager.sh
+++ create-cert-manager.sh
@@ -10,7 +10,7 @@ ${KUBE_CREATE} ns cert-manager
 #${KUBE_CREATE} -f manifests/volumes/cert-manager-pv.yaml
 
 ## Add the Jetstack Helm repository
-$ helm repo add jetstack https://charts.jetstack.io
+helm repo add jetstack https://charts.jetstack.io
 
 ## Install the cert-manager helm chart
-$ helm -n cert-manager install cert-manager jetstack/cert-manager
+helm -n cert-manager install cert-manager jetstack/cert-manager
