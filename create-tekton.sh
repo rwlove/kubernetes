c0e9b31eb5e7949ec2f99897e969fac050dd4f42
Robert Love <terp4life2001@gmail.com>
2020-08-01 14:26:30 -0700

    webhook

--- create-tekton.sh
+++ create-tekton.sh
@@ -16,4 +16,20 @@ ${KUBE_CREATE} -f manifests/tekton/release.yaml
 
 ${KUBE_CREATE} -f manifests/tekton/triggers-release.yaml
 
+${KUBE_CREATE} namespace getting-started
+
+${KUBE_CREATE} -f manifests/tekton/admin-role.yaml
+
+${KUBE_CREATE} -f manifests/tekton/webhook-role.yaml
+
 ${KUBE_CREATE} -f manifests/tekton/pipeline.yaml
+
+${KUBE_CREATE} -f manifests/tekton/triggers.yaml
+
+${KUBE_CREATE} -n getting-started -f manifests/tekton/create-ingress.yaml
+
+${KUBE_CREATE} -n getting-started -f manifests/tekton/create-webhook.yaml
+
+${KUBE_CREATE} manifests/tekton/ingress-run.yaml
+
+${KUBE_CREATE} manifests/tekton/webhook-run.yaml
--- destroy-tekton.sh
+++ destroy-tekton.sh
@@ -6,7 +6,23 @@ echo "############"
 echo "Delete Tekton Pipelines"
 echo "######"
 
+${KUBE_DELETE} manifests/tekton/webhook-run.yaml
+
+${KUBE_DELETE} manifests/tekton/ingress-run.yaml
+
+${KUBE_DELETE} -n getting-started -f manifests/tekton/create-webhook.yaml
+
+${KUBE_DELETE} -n getting-started -f manifests/tekton/create-ingress.yaml
+
+${KUBE_DELETE} -f manifests/tekton/triggers.yaml
+
 ${KUBE_DELETE} -f manifests/tekton/pipeline.yaml
+
+${KUBE_DELETE} -f manifests/tekton/webhook-role.yaml
+
+${KUBE_DELETE} -f manifests/tekton/admin-role.yaml
+
+${KUBE_DELETE} namespace getting-started
 
 ${KUBE_DELETE} -f manifests/tekton/triggers-release.yaml
 
