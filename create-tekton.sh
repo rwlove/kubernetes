f470a16399096a6c6c73013397c2790a323225b6
Robert Love <terp4life2001@gmail.com>
2020-08-02 07:13:04 -0700

    fix

--- create-tekton.sh
+++ create-tekton.sh
@@ -2,34 +2,48 @@
 
 KUBE_CREATE='kubectl create'
 
-${KUBE_CREATE} -f manifests/volumes/tekton-pv.yaml
-
-${KUBE_CREATE} configmap config-artifact-pvc \
-	       --from-literal=size=5GiB \
-	       --from-literal=storageClassName=tekton-data-storage-class
 
 echo "############"
 echo "Create Tekton Pipelines"
 echo "######"
 
+echo "-- Creating Tekton PV"
+${KUBE_CREATE} -f manifests/volumes/tekton-pv.yaml
+
+echo "-- Creating Tekton config-artifact-pvc ConfigMap"
+${KUBE_CREATE} configmap config-artifact-pvc \
+	       --from-literal=size=5GiB \
+	       --from-literal=storageClassName=tekton-data-storage-class
+
+echo "-- Creating Tekton Release"
 ${KUBE_CREATE} -f manifests/tekton/release.yaml
 
+echo "-- Creating Tekton Triggers Release"
 ${KUBE_CREATE} -f manifests/tekton/triggers-release.yaml
 
+echo "-- Creating getting-started namespace"
 ${KUBE_CREATE} namespace getting-started
 
+echo "-- Creating Tekton admin-role"
 ${KUBE_CREATE} -f manifests/tekton/admin-role.yaml
 
+echo "-- Creating Tekton webhook-role"
 ${KUBE_CREATE} -f manifests/tekton/webhook-role.yaml
 
+echo "-- Creating Tekton Pipeline"
 ${KUBE_CREATE} -f manifests/tekton/pipeline.yaml
 
+echo "-- Creating Tekton Triggers"
 ${KUBE_CREATE} -f manifests/tekton/triggers.yaml
 
+echo "-- Creating Tekton Ingress"
 ${KUBE_CREATE} -n getting-started -f manifests/tekton/create-ingress.yaml
 
+echo "-- Creating Tekton Webhook"
 ${KUBE_CREATE} -n getting-started -f manifests/tekton/create-webhook.yaml
 
+echo "-- Creating Tekton Ingress Run"
 ${KUBE_CREATE} -f manifests/tekton/ingress-run.yaml
 
+echo "-- Creating Tekton Webhook Run"
 ${KUBE_CREATE} -f manifests/tekton/webhook-run.yaml
--- destroy-tekton.sh
+++ destroy-tekton.sh
@@ -6,28 +6,41 @@ echo "############"
 echo "Delete Tekton Pipelines"
 echo "######"
 
+echo "-- Deleting Tekton Webhook Run"
 ${KUBE_DELETE} -f manifests/tekton/webhook-run.yaml
 
+echo "-- Deleting Tekton Ingress Run"
 ${KUBE_DELETE} -f manifests/tekton/ingress-run.yaml
 
+echo "-- Deleting Tekton Webhook"
 ${KUBE_DELETE} -n getting-started -f manifests/tekton/create-webhook.yaml
 
+echo "-- Deleting Tekton Ingress"
 ${KUBE_DELETE} -n getting-started -f manifests/tekton/create-ingress.yaml
 
+echo "-- Deleting Tekton Triggers"
 ${KUBE_DELETE} -f manifests/tekton/triggers.yaml
 
+echo "-- Deleting Tekton Pipeline"
 ${KUBE_DELETE} -f manifests/tekton/pipeline.yaml
 
+echo "-- Deleting Tekton Webhook Role"
 ${KUBE_DELETE} -f manifests/tekton/webhook-role.yaml
 
+echo "-- Deleting Tekton Admin Role"
 ${KUBE_DELETE} -f manifests/tekton/admin-role.yaml
 
+echo "-- Deleting getting-started namespace "
 ${KUBE_DELETE} namespace getting-started
 
+echo "-- Deleting Tekton Triggers Release"
 ${KUBE_DELETE} -f manifests/tekton/triggers-release.yaml
 
+echo "-- Deleting Tekton Release"
 ${KUBE_DELETE} -f manifests/tekton/release.yaml
 
+echo "-- Deleting Tekton config-artifact-pvc ConfigMap"
 ${KUBE_DELETE} configmap config-artifact-pvc
 
+echo "-- Deleting Tekton PV"
 ${KUBE_DELETE} -f manifests/volumes/tekton-pv.yaml
