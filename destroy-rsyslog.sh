e7bd8db23d0932593332f9d5b55089739bd03b13
Robert Love <terp4life2001@gmail.com>
2020-08-07 16:50:33 -0700

    fix rsyslog destroy script

--- destroy-rsyslog.sh
+++ destroy-rsyslog.sh
@@ -6,9 +6,9 @@ echo "############"
 echo "Delete rsyslog"
 echo "######"
 
-${KUBE_DELETE} -n rsyslog manifests/rsyslog/service.yaml
+${KUBE_DELETE} -n rsyslog -f manifests/rsyslog/service.yaml
 
-${KUBE_DELETE} -n rsyslog manifests/rsyslog/deployment.yaml
+${KUBE_DELETE} -n rsyslog -f manifests/rsyslog/deployment.yaml
 
 ${KUBE_DELETE} -f manifests/volumes/rsyslog-pv.yaml
 
