f20198f45763abcfd48ad6adb314d9862e738447
Robert Love <terp4life2001@gmail.com>
2021-02-24 11:35:30 -0500

    fix-volumes

--- create-radarr.sh
+++ create-radarr.sh
@@ -4,6 +4,8 @@ KUBE_CREATE='kubectl create'
 
 ${KUBE_CREATE} ns radarr
 
+${KUBE_CREATE} -f manifests/volumes/radarr-pv.yaml
+
 ${KUBE_CREATE} -f manifests/radarr/radarr-downloads-pvc.yaml
 
 echo "############"
