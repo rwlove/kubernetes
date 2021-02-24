5eb2f071a9a5640da611f62f717c38a9760891ff
Robert Love <terp4life2001@gmail.com>
2021-02-24 08:43:10 -0500

    use-media-for-son-rad

--- create-radarr.sh
+++ create-radarr.sh
@@ -4,7 +4,7 @@ KUBE_CREATE='kubectl create'
 
 ${KUBE_CREATE} ns radarr
 
-${KUBE_CREATE} -f manifests/radarr/radarr-downloads-pvc.yaml
+${KUBE_CREATE} -f manifests/volumes/radarr-pv.yaml
 
 echo "############"
 echo "Create radarr"
