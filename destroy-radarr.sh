4505062619538de2599fc870aee5e88eb533f583
Robert Love <terp4life2001@gmail.com>
2021-02-24 14:25:34 -0500

    debug-destroy

--- destroy-radarr.sh
+++ destroy-radarr.sh
@@ -3,11 +3,14 @@
 KUBE_DELETE='kubectl delete'
 
 echo "############"
-echo "Delete radarr"
+echo "Delete Radarr"
 echo "######"
 
+echo "## Uninstall Radarr Helm Chart"
 helm -n radarr uninstall radarr
 
+echo "## Delete Sonarr PV"
 ${KUBE_DELETE} -f manifests/volumes/radarr-pv.yaml
 
+echo "## Delete 'ronarr' Namespace"
 ${KUBE_DELETE} ns radarr
--- destroy-sonarr.sh
+++ destroy-sonarr.sh
@@ -3,11 +3,14 @@
 KUBE_DELETE='kubectl delete'
 
 echo "############"
-echo "Delete sonarr"
+echo "Delete Sonarr"
 echo "######"
 
+echo "## Uninstall Sonarr Helm Chart"
 helm -n sonarr uninstall sonarr
 
+echo "## Delete Sonarr PV"
 ${KUBE_DELETE} -f manifests/volumes/sonarr-pv.yaml
 
+echo "## Delete 'sonarr' Namespace"
 ${KUBE_DELETE} ns sonarr
