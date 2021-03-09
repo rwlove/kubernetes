e2e3491d3d2823e89e3b559c3aaee3458b68c601
Robert Love <terp4life2001@gmail.com>
2021-03-09 13:46:15 -0500

    fix home-services

--- create-home-services.sh
+++ create-home-services.sh
@@ -11,17 +11,17 @@ echo "############"
 echo "Create Physical (Storage) Volumes"
 echo "######"
 #${KUBE_CREATE} -f manifests/volumes/postgress-subsonic-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/subsonic-mysql-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/nextcloud-mysql-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/subsonic-mysql-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/nextcloud-mysql-pv.yaml
 #${KUBE_CREATE} -f manifests/volumes/mariadb-nextcloud-pv.yaml
 #${KUBE_CREATE} -f manifests/volumes/mariadb-subsonic-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/subsonic-music-volume-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/nextcloud-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/subsonic-music-volume-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/nextcloud-pv.yaml
 ${KUBE_CREATE} -f manifests/volumes/prometheus-server-pv.yaml
 ${KUBE_CREATE} -f manifests/volumes/prometheus-alertmanager-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/mythtv-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/kanboard-pv.yaml
-${KUBE_CREATE} -f manifests/volumes/biglybt-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/mythtv-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/kanboard-pv.yaml
+#${KUBE_CREATE} -f manifests/volumes/biglybt-pv.yaml
 
 echo "############"
 echo "Create namespaces"
@@ -33,14 +33,20 @@ ${KUBE_CREATE} namespace prometheus
 
 ./create-cert-manager.sh
 
+./create-qbittorrent.sh
+
 ./create-rabbitmq.sh
 
-echo "############"
-echo "Install postgress-operator"
-echo "######"
-${KUBE_CREATE} -f olm/postgres-operator.yaml
-${KUBE_CREATE} -f manifests/postgres/postgres-operator-ui.yaml
-${KUBE_CREATE} -n kanboard -f manifests/postgres/postgres-operator-crd.yaml
+./create-flaresolverr.sh
+
+./create-jellyfin.sh
+
+#echo "############"
+#echo "Install postgress-operator"
+#echo "######"
+#${KUBE_CREATE} -f olm/postgres-operator.yaml
+#${KUBE_CREATE} -f manifests/postgres/postgres-operator-ui.yaml
+#${KUBE_CREATE} -n kanboard -f manifests/postgres/postgres-operator-crd.yaml
 
 
 #echo "############"
@@ -50,23 +56,23 @@ ${KUBE_CREATE} -n kanboard -f manifests/postgres/postgres-operator-crd.yaml
 #helm repo add halkeye https://halkeye.github.io/helm-charts/
 #helm install -n mosquitto mosquitto halkeye/mosquitto --version 0.1.0 -f helm/mosquitto.yaml
 
-echo "############"
-echo "Create github-actions-runner"
-echo "######"
-helm repo add evryfs-oss https://evryfs.github.io/helm-charts/
-${KUBE_CREATE} namespace github-actions-runner
-helm install -n github-actions-runner github-actions-runner-operator evryfs-oss/github-actions-runner-operator
-${KUBE_CREATE} -f operators/github-actions-runner.yaml
+#echo "############"
+#echo "Create github-actions-runner"
+#echo "######"
+#helm repo add evryfs-oss https://evryfs.github.io/helm-charts/
+#${KUBE_CREATE} namespace github-actions-runner
+#helm install -n github-actions-runner github-actions-runner-operator evryfs-oss/github-actions-runner-operator
+#${KUBE_CREATE} -f operators/github-actions-runner.yaml
 
 echo "############"
 echo "Create Kanboard"
 echo "######"
 ${KUBE_CREATE} -f manifests/services/kanboard/kanboard.yaml
 
-echo "############"
-echo "Create BiglyBT"
-echo "######"
-${KUBE_CREATE} -f manifests/services/biglybt/biglybt.yaml
+#echo "############"
+#echo "Create BiglyBT"
+#echo "######"
+#${KUBE_CREATE} -f manifests/services/biglybt/biglybt.yaml
 
 ./create-jackett.sh
 
@@ -74,9 +80,17 @@ ${KUBE_CREATE} -f manifests/services/biglybt/biglybt.yaml
 
 ./create-sonarr.sh
 
+./create-lidarr.sh
+
 ./create-mpd.sh
 
-./create-mythtv-backend.sh
+./create-photoprism.sh
+
+./create-airsonic.sh
+
+
+
+#./create-mythtv-backend.sh
 
 #echo "############"
 #echo "Create kubemq"
@@ -166,23 +180,25 @@ ${KUBE_CREATE} -f manifests/services/biglybt/biglybt.yaml
 #echo "######"
 #kubectl create -f manifests/db/crunchy_postgresql/subsonic-db.yaml
 
-echo "############"
-echo "Install Node Red from Helm"
-echo "######"
-${KUBE_CREATE} -f manifests/node-red/node-red-namespace.yaml
+./create-node-red.sh
+
+#echo "############"
+#echo "Install Node Red from Helm"
+#echo "######"
+#${KUBE_CREATE} -f manifests/node-red/node-red-namespace.yaml
 #helm install -n node-red node-red -f helm/node-red.yaml stable/node-red
 
-./create-hass.sh
+#./create-hass.sh
 
-./create-grocy.sh
+#./create-grocy.sh
 
-./create-qbittorrent.sh
+#./create-qbittorrent.sh
 
-./create-qbittorrentvpn.sh
+#./create-qbittorrentvpn.sh
 
 ./create-speedtest.sh
 
-./create-mailu.sh
+#./create-mailu.sh
 
 ./create-heimdall.sh
 
@@ -196,30 +212,30 @@ echo "Install prometheus with helm"
 echo "######"
 helm install -n prometheus prometheus -f helm/prometheus.yaml stable/prometheus
 
-echo "############"
-echo "Create nextcloud with helm"
-echo "######"
-${KUBE_CREATE} -f manifests/services/nextcloud/nextcloud.yaml
-helm install -n nextcloud nextcloud -f helm/nextcloud.yaml --set nextcloud.username=admin,nextcloud.password=password,externalDatabase.type=mysql,externalDatabase.host=mysql-service,externalDatabase.database=nextcloud,externalDatabase.user=nextcloud,externalDatabase.password=password stable/nextcloud
+#echo "############"
+#echo "Create nextcloud with helm"
+#echo "######"
+#${KUBE_CREATE} -f manifests/services/nextcloud/nextcloud.yaml
+#helm install -n nextcloud nextcloud -f helm/nextcloud.yaml --set nextcloud.username=admin,nextcloud.password=password,externalDatabase.type=mysql,externalDatabase.host=mysql-service,externalDatabase.database=nextcloud,externalDatabase.user=nextcloud,externalDatabase.password=password stable/nextcloud
 
 ./create-pihole.sh
 
-echo "############"
-echo "Create nginx-hello"
-echo "######"
-${KUBE_CREATE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello.yaml
-${KUBE_CREATE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello-service.yaml
+#echo "############"
+#echo "Create nginx-hello"
+#echo "######"
+#${KUBE_CREATE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello.yaml
+#${KUBE_CREATE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello-service.yaml
 
-echo "############"
-echo "Create Subsonic"
-echo "######"
-${KUBE_CREATE} -f manifests/services/subsonic/subsonic.yaml
-${KUBE_CREATE} -f manifests/services/subsonic/subsonic-service.yaml
+#echo "############"
+#echo "Create Subsonic"
+#echo "######"
+#${KUBE_CREATE} -f manifests/services/subsonic/subsonic.yaml
+#${KUBE_CREATE} -f manifests/services/subsonic/subsonic-service.yaml
 
-echo "############"
-echo "Create MythTV Backend"
-echo "######"
-${KUBE_CREATE} -f manifests/services/mythtv/mythtv.yaml
+#echo "############"
+#echo "Create MythTV Backend"
+#echo "######"
+#${KUBE_CREATE} -f manifests/services/mythtv/mythtv.yaml
 
 echo "############"
 echo "Create k8sdash"
@@ -233,4 +249,4 @@ ${KUBE_CREATE} -f manifests/tools/dnsutils.yaml
 
 ./create-metallb.sh
 
-./create-nginx-ingress.sh
+#./create-nginx-ingress.sh
--- destroy-home-services.sh
+++ destroy-home-services.sh
@@ -3,8 +3,6 @@
 KUBE_DELETE='kubectl delete'
 NAMESPACE='-n home-services'
 
-./destroy-nginx-ingress.sh
-
 ./destroy-metallb.sh
 
 echo "############"
@@ -17,7 +15,7 @@ echo "Delete k8dash"
 echo "######"
 ${KUBE_DELETE} -f manifests/dashboard/kubernetes-k8dash.yaml
 
-./destroy-mythtv-backend.sh
+#./destroy-mythtv-backend.sh
 
 ./destroy-mpd.sh
 
@@ -25,32 +23,38 @@ ${KUBE_DELETE} -f manifests/dashboard/kubernetes-k8dash.yaml
 
 ./destroy-radarr.sh
 
+./destroy-lidarr.sh
+
+./destroy-airsonic.sh
+
+./destroy-photoprism.sh
+
 ./destroy-jackett.sh
 
-echo "############"
-echo "Delete nginx-hello"
-echo "######"
-${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello-service.yaml
-${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello.yaml
+#echo "############"
+#echo "Delete nginx-hello"
+#echo "######"
+#${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello-service.yaml
+#${KUBE_DELETE} ${NAMESPACE} -f manifests/services/nginx-hello/nginx-hello.yaml
 
 ./destroy-pihole.sh
 
-echo "############"
-echo "Delete MythTV Backend"
-echo "######"
-${KUBE_DELETE} -f manifests/services/mythtv/mythtv.yaml
+#echo "############"
+#echo "Delete MythTV Backend"
+#echo "######"
+#${KUBE_DELETE} -f manifests/services/mythtv/mythtv.yaml
 
-echo "############"
-echo "Delete Subsonic"
-echo "######"
-${KUBE_DELETE} -f manifests/services/subsonic/subsonic-service.yaml
-${KUBE_DELETE} -f manifests/services/subsonic/subsonic.yaml
+#echo "############"
+#echo "Delete Subsonic"
+#echo "######"
+#${KUBE_DELETE} -f manifests/services/subsonic/subsonic-service.yaml
+#${KUBE_DELETE} -f manifests/services/subsonic/subsonic.yaml
 
-echo "############"
-echo "Delete Nextcloud"
-echo "######"
-helm uninstall -n nextcloud nextcloud
-${KUBE_DELETE} -f manifests/services/nextcloud/nextcloud.yaml
+#echo "############"
+#echo "Delete Nextcloud"
+#echo "######"
+#helm uninstall -n nextcloud nextcloud
+#${KUBE_DELETE} -f manifests/services/nextcloud/nextcloud.yaml
 
 #echo "############"
 #echo "Delete MySQL"
@@ -77,15 +81,17 @@ helm uninstall ${NAMESPACE} grafana
 
 ./destroy-qbittorrent.sh
 
-./destroy-grocy.sh
+#./destroy-grocy.sh
 
-./destroy-hass.sh
+#./destroy-hass.sh
 
-echo "############"
-echo "Uninstall Node Red"
-echo "######"
+#echo "############"
+#echo "Uninstall Node Red"
+#echo "######"
 #helm -n node-red uninstall node-red
-${KUBE_DELETE} -f manifests/node-red/node-red-namespace.yaml
+#${KUBE_DELETE} -f manifests/node-red/node-red-namespace.yaml
+
+./destroy-node-red.sh
 
 #echo "############"
 #echo "Delete the subsonic PostgreSQL database"
@@ -104,23 +110,23 @@ ${KUBE_DELETE} -f manifests/node-red/node-red-namespace.yaml
 #${KUBE_DELETE} -f manifests/services/kubemq/kubemq-cluster.yaml
 #${KUBE_DELETE} -f operators/kubemq-operator.yaml
 
-echo "############"
-echo "Delete BiglyBT"
-echo "######"
-${KUBE_DELETE} -f manifests/services/biglybt/biglybt.yaml
+#echo "############"
+#echo "Delete BiglyBT"
+#echo "######"
+#${KUBE_DELETE} -f manifests/services/biglybt/biglybt.yaml
 
 echo "############"
 echo "Delete Kanboard"
 echo "######"
 ${KUBE_DELETE} -f manifests/services/kanboard/kanboard.yaml
 
-echo "############"
-echo "Delete github-actions-runner"
-echo "######"
-${KUBE_DELETE} -f operators/github-actions-runner.yaml
-helm uninstall -n github-actions-runner github-actions-runner-operator
-${KUBE_DELETE} namespace github-actions-runner
-helm repo remove evryfs-oss
+#echo "############"
+#echo "Delete github-actions-runner"
+#echo "######"
+#${KUBE_DELETE} -f operators/github-actions-runner.yaml
+#helm uninstall -n github-actions-runner github-actions-runner-operator
+#${KUBE_DELETE} namespace github-actions-runner
+#helm repo remove evryfs-oss
 
 #echo "############"
 #echo "Delete kalkeye helm repo and install mosquitto"
@@ -130,11 +136,11 @@ helm repo remove evryfs-oss
 #helm repo remove halkeye
 #${KUBE_DELETE} -f manifests/mosquitto/mosquitto-namespace.yaml
 
-echo "############"
-echo "Delete postgress-operator"
-echo "######"
-${KUBE_DELETE} -f manifests/postgres/postgres-operator-ui.yaml
-${KUBE_DELETE} -f olm/postgres-operator.yaml
+#echo "############"
+#echo "Delete postgress-operator"
+#echo "######"
+#${KUBE_DELETE} -f manifests/postgres/postgres-operator-ui.yaml
+#${KUBE_DELETE} -f olm/postgres-operator.yaml
 
 ./destroy-rabbitmq.sh
 
@@ -152,19 +158,19 @@ ${KUBE_DELETE} -f manifests/services/home-services-namespace.yaml
 echo "############"
 echo "Delete Physical (storage) Volumes"
 echo "######"
-${KUBE_DELETE} -f manifests/volumes/biglybt-pv.yaml
+#${KUBE_DELETE} -f manifests/volumes/biglybt-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/kanboard-pv.yaml
-${KUBE_DELETE} -f manifests/volumes/mythtv-pv.yaml
+#${KUBE_DELETE} -f manifests/volumes/mythtv-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/prometheus-alertmanager-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/prometheus-server-pv.yaml
-${KUBE_DELETE} -f manifests/volumes/nextcloud-pv.yaml
+#${KUBE_DELETE} -f manifests/volumes/nextcloud-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/mpd-music-volume-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/subsonic-music-volume-pv.yaml
 #${KUBE_DELETE} -f manifests/volumes/mariadb-nextcloud-pv.yaml
 #${KUBE_DELETE} -f manifests/volumes/mariadb-subsonic-pv.yaml
 #${KUBE_DELETE} -f manifests/volumes/postgress-subsonic-pv.yaml
-${KUBE_DELETE} -f manifests/volumes/nextcloud-mysql-pv.yaml
-${KUBE_DELETE} -f manifests/volumes/subsonic-mysql-pv.yaml
+#${KUBE_DELETE} -f manifests/volumes/nextcloud-mysql-pv.yaml
+#${KUBE_DELETE} -f manifests/volumes/subsonic-mysql-pv.yaml
 ${KUBE_DELETE} -f manifests/volumes/rabbitmq-pv.yaml
 
 #${KUBE_DELETE} -f manifests/dns/external-dns/external-dns.yaml
